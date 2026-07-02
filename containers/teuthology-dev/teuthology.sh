fc945f93f25d1b91cded184c8393fdb2870e5243
actuallychris-akamai <chsmith@akamai.com>
2026-07-02 10:43:21 -0400

    Enhance filter-out logic in teuthology.sh
    Refactor filter-out handling in teuthology.sh to merge default and user-provided values, allowing for dynamic filtering based on extra arguments.

--- containers/teuthology-dev/teuthology.sh
+++ containers/teuthology-dev/teuthology.sh
@@ -16,6 +16,75 @@ if [ "$TEUTHOLOGY_SUITE" != "none" ]; then
     if [ -n "$TEUTHOLOGY_BRANCH" ]; then
       TEUTH_BRANCH_FLAG="--teuthology-branch $TEUTHOLOGY_BRANCH"
     fi
+
+    # Merge default and user-provided filter-out values into a single flag.
+    DEFAULT_FILTER_OUT="${TEUTHOLOGY_DEFAULT_FILTER_OUT:-libcephfs,kclient,valgrind,cls_sem_set}"
+    EXTRA_ARGS_RAW="${TEUTHOLOGY_SUITE_EXTRA_ARGS:-}"
+    EXTRA_ARGS_NO_FILTER_OUT=()
+    EXTRA_FILTER_OUT=""
+
+    if [ -n "$EXTRA_ARGS_RAW" ]; then
+        read -r -a EXTRA_ARGS_ARR <<< "$EXTRA_ARGS_RAW"
+        i=0
+        while [ $i -lt ${#EXTRA_ARGS_ARR[@]} ]; do
+            arg="${EXTRA_ARGS_ARR[$i]}"
+            if [ "$arg" = "--filter-out" ]; then
+                i=$((i + 1))
+                if [ $i -lt ${#EXTRA_ARGS_ARR[@]} ]; then
+                    if [ -n "$EXTRA_FILTER_OUT" ]; then
+                        EXTRA_FILTER_OUT="$EXTRA_FILTER_OUT,${EXTRA_ARGS_ARR[$i]}"
+                    else
+                        EXTRA_FILTER_OUT="${EXTRA_ARGS_ARR[$i]}"
+                    fi
+                fi
+            elif [[ "$arg" == --filter-out=* ]]; then
+                value="${arg#--filter-out=}"
+                if [ -n "$value" ]; then
+                    if [ -n "$EXTRA_FILTER_OUT" ]; then
+                        EXTRA_FILTER_OUT="$EXTRA_FILTER_OUT,$value"
+                    else
+                        EXTRA_FILTER_OUT="$value"
+                    fi
+                fi
+            else
+                EXTRA_ARGS_NO_FILTER_OUT+=("$arg")
+            fi
+            i=$((i + 1))
+        done
+    fi
+
+    MERGED_FILTER_OUT="$DEFAULT_FILTER_OUT"
+    if [ -n "$EXTRA_FILTER_OUT" ]; then
+        MERGED_FILTER_OUT="$MERGED_FILTER_OUT,$EXTRA_FILTER_OUT"
+    fi
+
+    FILTER_OUT_FINAL=""
+    OLD_IFS="$IFS"
+    IFS=','
+    for raw_keyword in $MERGED_FILTER_OUT; do
+        keyword="${raw_keyword#"${raw_keyword%%[![:space:]]*}"}"
+        keyword="${keyword%"${keyword##*[![:space:]]}"}"
+        if [ -z "$keyword" ]; then
+            continue
+        fi
+        case ",$FILTER_OUT_FINAL," in
+            *",$keyword,"*) ;;
+            *)
+                if [ -n "$FILTER_OUT_FINAL" ]; then
+                    FILTER_OUT_FINAL="$FILTER_OUT_FINAL,$keyword"
+                else
+                    FILTER_OUT_FINAL="$keyword"
+                fi
+                ;;
+        esac
+    done
+    IFS="$OLD_IFS"
+
+    FILTER_OUT_ARGS=()
+    if [ -n "$FILTER_OUT_FINAL" ]; then
+        FILTER_OUT_ARGS=(--filter-out "$FILTER_OUT_FINAL")
+    fi
+
     teuthology-suite -v \
         $TEUTH_BRANCH_FLAG \
         -m "$TEUTHOLOGY_MACHINE_TYPE" \
@@ -25,10 +94,10 @@ if [ "$TEUTHOLOGY_SUITE" != "none" ]; then
         --suite "${TEUTHOLOGY_SUITE:-teuthology:no-ceph}" \
         --suite-branch "${TEUTHOLOGY_SUITE_BRANCH:-main}" \
         --suite-repo "${TEUTHOLOGY_SUITE_REPO:-https://github.com/ceph/ceph.git}" \
-        --filter-out "libcephfs,kclient,valgrind,cls_sem_set" \
+        "${FILTER_OUT_ARGS[@]}" \
         --force-priority \
         --seed 349 \
-        ${TEUTHOLOGY_SUITE_EXTRA_ARGS} \
+        "${EXTRA_ARGS_NO_FILTER_OUT[@]}" \
         $TEUTHOLOGY_CONF
     DISPATCHER_EXIT_FLAG='--exit-on-empty-queue'
     teuthology-queue -m $TEUTHOLOGY_MACHINE_TYPE -s | \
